b0648a5d718306fbb869e613e63d964c5299f784
Carlos Eduardo Arantes Ferreira <carantes@mobistart.com.br>
2019-02-12 09:25:30 -0200

    setup inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './molecules/TodoList'
+
+// Todo: Redux
+const todos = [
+  { id: 1, text: 'Clean the house' },
+  { id: 2, text: 'Study English'}
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <TodoList todos={todos} onTodoClick={(id) => console.log(id)} />
     );
   }
 }
---
+++ src/atoms/Todo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Todo = ({onClick, completed = false, text}) => (
+  <li
+    onClick={onClick}
+    style={{
+      textDecoration: completed ? 'line-through' : 'none'
+    }}
+  >
+    {text}
+  </li>
+)
+
+Todo.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired
+}
+
+export default Todo
